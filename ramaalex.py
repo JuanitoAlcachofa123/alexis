a5872cc46895533dbcf0b318c4c74e4a1ef33bfb
JuanitoAlcachofa123 <juanchavezMGmaster@gmail.com>
2024-07-24 09:53:08 -0400

    rama de alex

---
+++ ramaalex.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese dos números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Calcular la suma de los dos números
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de", numero1, "y", numero2, "es:", suma)
